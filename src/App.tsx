f516086db30ba408331001845e86abf5ed002702
Sergey Kuznetsov <mr.enline@gmail.com>
2022-06-14 19:26:36 +0300

    add css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./components/login/Login";
 
 function App() {
     return (
-        <div className="App">
-            <Login />
-        </div>
+        <Router>
+            <div className="App">
+                <Login />
+                <Routes>
+                    <Route path="/" element={<App />} />
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,13 +1,35 @@
 import React from "react";
 import styles from "./Login.module.css";
 import Logo from "../assets/img/finnplay-logo.svg";
+import VisiblePassword from "../assets/img/visible-password.svg";
 
 const Login = () => {
     return (
         <div className={styles.login}>
             <div className={styles.loginform}>
-                <img className={styles.logo} src={Logo} alt="finnplay-logo" />
-                <input type="text" />
+                <img
+                    className={styles.loginform__logo}
+                    src={Logo}
+                    alt="finnplay-logo"
+                />
+                <div className={styles.inputs__textfield}>
+                    <div className={styles.textfield_content}>
+                        <input type="text" name="login" id="login" required />
+                        <label htmlFor="login">Логин</label>
+                    </div>
+                </div>
+                <div className={styles.inputs__textfield}>
+                    <div className={styles.textfield_content}>
+                        <input type="password" name="pass" id="pass" required />
+                        <label htmlFor="pass">Пароль</label>
+                    </div>
+                    <img
+                        className={styles.icons__showpassword}
+                        src={VisiblePassword}
+                        alt="visible-password"
+                    />
+                </div>
+                <button className={styles.loginform__button}>Login</button>
             </div>
         </div>
     );
